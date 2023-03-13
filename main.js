cd194363443ea6ca2b8988d38eddd49f940ae111
HOSSAM-ELDIN <HOS-ELDIN@users.noreply.github.com>
2023-03-13 10:00:48 +0200

    js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-let nav = document.querySelectorAll("nav ul a")
+let nav = document.querySelectorAll("nav ul a");
+let burger = document.getElementById("burger");
+let closeMenu = document.getElementById("close");
 
-nav.forEach((a)=>{
-  a.addEventListener("click", ()=>{
-nav.forEach((link)=>{
-  link.classList.remove("active")
-})
-a.classList.add("active")
-  })
-})
+nav.forEach((a) => {
+	a.addEventListener("click", () => {
+		nav.forEach((link) => {
+			link.classList.remove("active");
+		});
+		a.classList.add("active");
+	});
+});
+
+burger.onclick = () => {
+	burger.classList.toggle("hidden");
+	closeMenu.classList.toggle("hidden");
+};
+closeMenu.onclick = () => {
+	burger.classList.toggle("hidden");
+	closeMenu.classList.toggle("hidden");
+};
