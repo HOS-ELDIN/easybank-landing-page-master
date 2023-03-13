276ab167ee3e860c75cd3f3e5ce48491eb288c35
HOSSAM-ELDIN <HOS-ELDIN@users.noreply.github.com>
2023-03-13 09:37:01 +0200

    good

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,23 +9,27 @@ module.exports = {
 			colors: {
 				// ### Primary
 
-				"DarkBlue": "hsl(233, 26%, 24%)",
-				"LimeGreen": "hsl(136, 65%, 51%)",
-				"BrightCyan": "hsl(192, 70%, 51%)",
+				DarkBlue: "hsl(233, 26%, 24%)",
+				LimeGreen: "hsl(136, 65%, 51%)",
+				BrightCyan: "hsl(192, 70%, 51%)",
 
 				// ### Neutral
 
-				"GrayishBlue": "hsl(233, 8%, 62%)", // nav text
-				"LightGrayishBlue": "hsl(220, 16%, 96%)", // dark esction bg
-				"VeryLightGray": "hsl(0, 0%, 98%)", //bright section bg
-				"White": "hsl(0, 0%, 100%)",
+				GrayishBlue: "hsl(233, 8%, 62%)", // nav text
+				LightGrayishBlue: "hsl(220, 16%, 96%)", // dark esction bg
+				VeryLightGray: "hsl(0, 0%, 98%)", //bright section bg
+				White: "hsl(0, 0%, 100%)",
 
 				"-atribution": "hsl(228, 45%, 44%)",
 			},
-      backgroundImage: {
-				"desk": "url(../images/bg-intro-desktop.svg)",
-				"mobile": "url(../images/bg-intro-mobile.svg)",
-			}
+			backgroundImage: {
+				desk: "url(../images/bg-intro-desktop.svg)",
+				mobile: "url(../images/bg-intro-mobile.svg)",
+			},
+			gridTemplateColumns: {
+				fourResponsive: "repeat(auto-fit, minmax(280px,1fr))",
+				footer: "repeat(auto-fit, minmax(280px,1fr))",
+			},
 		},
 	},
 	plugins: [],
